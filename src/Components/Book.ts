d142833968ce05c0bb2b04d0196a45628a3d650d
Yvan Douënel <y.douenel@coopernet.fr>
2023-09-11 13:48:26 +0200

    Début de fonctionnement de l'affichage des books.

--- src/Components/Book.ts
+++ src/Components/Book.ts
@@ -2,15 +2,27 @@ import createMarkup from "../utils/domManagement";
 export default class Book {
   private id: number;
   private title: string;
-  private book_section:HTMLElement;
-  constructor(book_section:HTMLElement, id:number, title:string){
+  private book_section: HTMLElement;
+  private like;
+  constructor(book_section: HTMLElement, id: number, title: string) {
     this.book_section = book_section;
     this.id = id;
     this.title = title;
-    this.render();
+    this.like = this.render();
+    this.handleEvents();
   }
   private render() {
-    const section_book = createMarkup("section","",this.book_section);
-    const title_book = createMarkup("h2",this.title,section_book);
+    const section_book = createMarkup("section",
+      "",
+      this.book_section,
+      [{ class: "d-flex justify-content-between my-2" }]);
+    const title_book = createMarkup("h2", this.title, section_book);
+    const like = createMarkup("button", "J'aime", section_book, [{ class: "btn btn-success" }]);
+    return like;
+  }
+  private handleEvents() {
+    this.like.onclick = () => {
+      console.log(`Like cliqué`);
+    }
   }
 }
